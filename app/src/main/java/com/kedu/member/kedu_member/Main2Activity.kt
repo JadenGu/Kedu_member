679f78517caedcac7b6afe92d10b4f42da57ba0e
JadenGu <247760733@qq.com>
2017-12-13 16:49:59 +0800

    Different build types.

---
+++ app/src/main/java/com/kedu/member/kedu_member/Main2Activity.kt
@@ -0,0 +1,39 @@
+package com.kedu.member.kedu_member
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    private val url = BuildConfig.BASE_URL + BuildConfig.SUFFIX
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        webview.loadUrl(url)
+        webview.isVerticalScrollBarEnabled = false
+        webview.settings.domStorageEnabled = true
+        webview.settings.javaScriptEnabled = true
+        webview.settings.useWideViewPort = true
+
+        webview.webViewClient = MyWebViewClient()
+    }
+
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    inner class MyWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            return true
+        }
+    }
+}
